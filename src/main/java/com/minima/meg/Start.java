a69469c5cfba10863f00b32bb8a245ebef4f4c41
spartacusrex <patrickcerri@gmail.com>
2025-03-07 10:42:07 +0000

    latest 2.4 jar

--- src/main/java/com/minima/meg/Start.java
+++ src/main/java/com/minima/meg/Start.java
@@ -26,7 +26,7 @@ public class Start
 	/**
 	 * The Main MEG Manager
 	 */
-	public static final String MEG_VERSION = "2.3";
+	public static final String MEG_VERSION = "2.4";
 	
 	private static MEGManager mMEG;
 	
