30287406ec988e6aecb0fa212dc93e0d50528648
spartacusrex <patrickcerri@gmail.com>
2026-05-17 10:39:14 +0100

    convert all sql to prepared statements

--- src/main/java/com/minima/meg/database/LogsDB.java
+++ src/main/java/com/minima/meg/database/LogsDB.java
@@ -1,5 +1,7 @@
 package com.minima.meg.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -8,6 +10,9 @@ import org.json.JSONObject;
 import com.minima.meg.utils.SqlDB;
 
 public class LogsDB extends SqlDB {
+	
+	PreparedStatement SQL_INSERT_LOG 	= null;
+	PreparedStatement SQL_SELECT_LOGS 	= null;
 	
 	public LogsDB() {
 		super();
@@ -32,18 +37,55 @@ public class LogsDB extends SqlDB {
 		
 		//Close connection
 		stmt.close();
+		
+		SQL_INSERT_LOG 	= mSQLConnection.prepareStatement("INSERT INTO logs(event, details, username, created) VALUES (?,?,?,?)");
+		SQL_SELECT_LOGS	= mSQLConnection.prepareStatement("SELECT * FROM logs ORDER BY ID DESC LIMIT ? OFFSET ?");
 	}
 
-
 	public JSONObject addLog(String zEvent, String zDetails, String zUser) {
-		String sql = "INSERT INTO logs(event, details, username, created) VALUES "
-				+ "('"+zEvent+"','"+zDetails+"','"+zUser+"',"+System.currentTimeMillis()+")";
 		
-		return executeGenericSQL(sql);
+		try {
+			
+			//Set Params
+			SQL_INSERT_LOG.clearParameters();
+			SQL_INSERT_LOG.setString(1, zEvent);
+			SQL_INSERT_LOG.setString(2, zDetails);
+			SQL_INSERT_LOG.setString(3, zUser);
+			SQL_INSERT_LOG.setLong(4, System.currentTimeMillis());
+			
+			//Run the query
+			SQL_INSERT_LOG.execute();
+			
+			return SqlDB.getSQLSuccessUpdateJSON();
+			
+		} catch (SQLException e) {
+			return SqlDB.getSQLErrorJSON(e);
+		}
+		
+		//String sql = "INSERT INTO logs(event, details, username, created) VALUES "
+		//		+ "('"+zEvent+"','"+zDetails+"','"+zUser+"',"+System.currentTimeMillis()+")";
+		//return executeGenericSQL(sql);
 	}
 	
 	public JSONObject getLogs(int zSize, int zOffset){
-		return executeGenericSQL("SELECT * FROM logs ORDER BY ID DESC LIMIT "+zSize+" OFFSET "+zOffset);
+		
+		try {
+			
+			//Set Params
+			SQL_SELECT_LOGS.clearParameters();
+			SQL_SELECT_LOGS.setInt(1, zSize);
+			SQL_SELECT_LOGS.setInt(2, zOffset);
+			
+			//Run the query
+			ResultSet rs = SQL_SELECT_LOGS.executeQuery();
+			
+			return SqlDB.convertResToJSON(rs);
+			
+		} catch (SQLException e) {
+			return SqlDB.getSQLErrorJSON(e);
+		}
+		
+		//return executeGenericSQL("SELECT * FROM logs ORDER BY ID DESC LIMIT "+zSize+" OFFSET "+zOffset);
 	}
 	
 }
--- src/main/java/com/minima/meg/utils/SqlDB.java
+++ src/main/java/com/minima/meg/utils/SqlDB.java
@@ -343,4 +343,86 @@ public abstract class SqlDB {
 		
 		return results;
 	}
+	
+	public static JSONObject convertResToJSON(ResultSet zRes) {
+		
+		JSONObject results = new JSONObject();
+		
+		try {
+			//The data arrays
+			JSONArray allrows      = new JSONArray();
+			
+			//Get the Headers..
+			ResultSetMetaData rsmd = zRes.getMetaData();
+			int columnnum          = rsmd.getColumnCount();
+			
+			//Get the Results..
+			int counter=0;
+			while(zRes.next()) {
+				counter++;
+				JSONObject row = new JSONObject();
+				for(int i=1;i<=columnnum;i++) {
+					String column = rsmd.getColumnName(i);
+					Object obj    = zRes.getObject(i);
+					
+					//Make sure NOT NULL - or Omit.. 
+					if(obj!=null) {
+						//Treat some type special
+						if(rsmd.getColumnClassName(i).equals("java.sql.Clob")) {
+							java.sql.Clob clob = (java.sql.Clob)obj;
+	                    	String strvalue = clob.getSubString(1, (int) clob.length());
+	                    	row.put(column, strvalue);
+						
+						}else {
+							row.put(column, obj.toString());
+						}
+					}
+				}
+				allrows.put(row);
+			}
+			
+			//There are results..
+			results.put("status", true);
+			results.put("results", true);
+			results.put("count",counter);
+			results.put("rows", allrows);
+		
+		} catch (SQLException e) {
+			Log.log(e.toString());
+			
+			results.put("status", false);
+			results.put("count",0);
+			results.put("rows", new JSONArray());
+			results.put("results", false);
+			results.put("error", e.toString());
+		}
+		
+		//Log.log("CONV:"+results.toString());
+		
+		return results;
+	}
+	
+	public static JSONObject getSQLErrorJSON(Exception zEXC) {
+		Log.log(zEXC.toString());
+		
+		JSONObject results = new JSONObject();
+		
+		results.put("status", false);
+		results.put("count",0);
+		results.put("rows", new JSONArray());
+		results.put("results", false);
+		results.put("error", zEXC.toString());
+		
+		return results;
+	}
+	
+	public static JSONObject getSQLSuccessUpdateJSON() {
+		
+		JSONObject results = new JSONObject();
+		
+		results.put("status", true);
+		results.put("results", false);
+		
+		return results;
+	}
 }
